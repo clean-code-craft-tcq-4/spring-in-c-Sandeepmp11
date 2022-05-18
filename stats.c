2930fc25547ee851c0b509dad3f43a2b3005dea6
Sandeepmp11 <105204173+Sandeepmp11@users.noreply.github.com>
2022-05-18 23:16:16 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -9,3 +9,37 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+
+int main()
+{
+    int i,sum=0;
+    float numberset[];
+    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    for(i=0; i<(setlength-1);i++)
+    {
+        sum = sum + numberset[i];
+    }
+    s.average = sum/setlength;
+    
+    printf("Average = %f",s.average);
+    
+    s.min = numberseet[0];
+    for(i=0; i<(setlength-1);i++)
+    {
+        if(numberset[i] < s.min)
+        {
+            s.min = numberset[i];
+        }
+    }
+    printf("Minimum = %f",s.min);
+    
+    s.max = numberseet[0];
+    for(i=0; i<(setlength-1);i++)
+    {
+        if(numberset[i] > s.max)
+        {
+            s.max = numberset[i];
+        }
+    }
+    printf("Maximum = %f",s.max);
+    
