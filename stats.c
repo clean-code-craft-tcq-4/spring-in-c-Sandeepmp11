2ada8701b7d69e3035e2710632e77e50be4a7903
Sandeepmp11 <105204173+Sandeepmp11@users.noreply.github.com>
2022-05-19 07:03:17 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,15 +1,20 @@
 #include "stats.h"
 #include <cstdio>
+struct Stats{
+    float average;
+    float min;
+    float max;
+}
+
+struct Stats compute_statistics(const float* numberset, int setlength);
 
 int main()
 {
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
+struct Stats s;
+    s = compute_statistics(const float* numberset, int setlength);
     s.average = 0;
     s.min = 0;
-    s.max = 0;
-};
-
+    s.max = 0; 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 return 0;
@@ -17,24 +22,24 @@ return 0;
 
 struct Stats compute_statistics(const float* numberset, int setlength)
 {
+    struct Stats s1;
     int i;
     float sum=0;
-    float numberset[100];
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    setlength = sizeof(numberset) / sizeof(numberset[0]);
     for(i=0; i<(setlength-1);i++)
     {
         sum = sum + numberset[i];
     }
-    s.average = sum/setlength;
+    s1.average = sum/setlength;
     
     printf("Average = %f",s.average);
     
-    s.min = numberset[0];
+    s1.min = numberset[0];
     for(i=0; i<(setlength-1);i++)
     {
-        if(numberset[i] < s.min)
+        if(numberset[i] < s1.min)
         {
-            s.min = numberset[i];
+            s1.min = numberset[i];
         }
     }
     printf("Minimum = %f",s.min);
@@ -42,11 +47,11 @@ struct Stats compute_statistics(const float* numberset, int setlength)
     s.max = numberset[0];
     for(i=0; i<(setlength-1);i++)
     {
-        if(numberset[i] > s.max)
+        if(numberset[i] > s1.max)
         {
-            s.max = numberset[i];
+            s1.max = numberset[i];
         }
     }
-    printf("Maximum = %f",s.max);
     
+    return s1;
 }
